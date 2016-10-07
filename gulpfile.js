32e7937c92d4901f51b583a5f0cc2214d22192fb
k14041kk <k14041kk@aitech.ac.jp>
2016-10-07 20:54:20 +0900

    追加：ビルドの設定

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var requireDir = require('require-dir');
+var tasks = requireDir('./gulp/tasks');
+var gulp = require('gulp');
+
+gulp.task('default', function() {
+	console.log("################################");
+	console.log("     SystemEngineeringTeam    ");
+	console.log("  http://set1.ie.aitech.ac.jp/ ");
+	console.log("################################");
+	console.log("  Powered by Gulp  ");
+	console.log("  by: Kamiya  ");
+	console.log("################################");
+	console.log("  Commands ");
+	console.log("  npm install");
+	console.log("  gulp build");
+	console.log("################################");
+});
