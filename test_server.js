5fadf1beef24253b82efa3a9346c5d4a691447ed
k14041kk <k14041kk@aitech.ac.jp>
2016-10-10 17:26:44 +0900

    修正：テストサーバー

--- test_server.js
+++ test_server.js
@@ -1,6 +1,6 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-var app = connect().use(serveStatic('.dist/'));
+var app = connect().use(serveStatic('./dist/'));
 require('http').createServer(app).listen(8080);
 
 console.log('Server running at http://127.0.0.1:8080/');
