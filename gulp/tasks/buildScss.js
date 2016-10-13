eb16efac6321ed82b8b8476c5e9faf1b60cb4895
k14041kk <k14041kk@aitech.ac.jp>
2016-10-13 16:27:18 +0900

    追加：CSSの整理

--- gulp/tasks/buildScss.js
+++ gulp/tasks/buildScss.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var csscomb = require('gulp-csscomb');
 var autoprefixer = require('gulp-autoprefixer');
  
 gulp.task('buildScss', function () {
@@ -7,6 +8,7 @@ gulp.task('buildScss', function () {
   return gulp.src('./src/main/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
+    .pipe(csscomb())
     .pipe(gulp.dest('./dist/css'));
   
 });
