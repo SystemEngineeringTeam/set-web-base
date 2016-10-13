47ae8a5054738f407220f7fd0fb8f87fbd7ca159
k14041kk <k14041kk@aitech.ac.jp>
2016-10-13 16:10:28 +0900

    追加：CSSベンダープレフィックス

--- gulp/tasks/buildScss.js
+++ gulp/tasks/buildScss.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
  
 gulp.task('buildScss', function () {
 	
   return gulp.src('./src/main/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
     .pipe(gulp.dest('./dist/css'));
   
 });
