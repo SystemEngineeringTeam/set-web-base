202a56874ddd85980a8a9e608c15056b0ad31792
k14041kk <k14041kk@aitech.ac.jp>
2016-10-15 16:44:37 +0900

    変更：jsを外部ファイルに

---
+++ src/main/js/swiper.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+	
+	var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        calculateHeight:true,
+        paginationClickable: true,
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: 2500,
+        autoplayDisableOnInteraction: false,
+        loop:true
+	});
+	
+});
