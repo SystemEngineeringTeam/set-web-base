0bf5179735405ea95404ff92252525f385cb9eba
k14041kk <k14041kk@aitech.ac.jp>
2016-10-19 10:37:57 +0900

    追加：htmlを整形する機能

--- gulp/tasks/buildEJS.js
+++ gulp/tasks/buildEJS.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp');
 var ejs = require("gulp-ejs");
+var gulpPrettify = require("gulp-prettify");
 
 gulp.task('buildEJS', function() {
 
     var ejs_src = ["./src/main/ejs/**/*.ejs", "!./src/main/ejs/**/_*.ejs"];
 	gulp.src(ejs_src)
 	.pipe(ejs({},{"ext": ".html"}))
+	.pipe(gulpPrettify())
 	.pipe(gulp.dest("./dist/"));
 
 });
